553e1dff41cdca9398a53d69731054e23470a0d8
Robert Viramontes <rviramontes24@gmail.com>
2021-12-01 21:38:37 +0000

    Basic gym env for ordernet.

---
+++ src/drt/orderNet/OrderNetEnv.py
@@ -0,0 +1,20 @@
+from gym import Env
+
+
+class OrderNetEnv(Env):
+    def __init__(self):
+        """
+        Environment for OrderNet RL.
+        """
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
