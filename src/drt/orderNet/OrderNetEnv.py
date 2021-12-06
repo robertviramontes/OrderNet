82cc13731e62cc2ecf2cbf7a85a8b8f568f62d75
Robert Viramontes <rviramontes24@gmail.com>
2021-12-06 06:48:35 +0000

    Further build out the OrderNet envrionment.

--- src/drt/orderNet/OrderNetEnv.py
+++ src/drt/orderNet/OrderNetEnv.py
@@ -22,19 +22,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import subprocess
+from typing import Tuple
 from gym import Env, spaces
 import numpy as np
+from numpy.core.fromnumeric import std
 from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
+import zmq
+import random
 
 class OrderNetEnv(Env):
     """
     Implements the custom OrderNet learning environment for OpenAI Gym.
     """
 
-    def __init__(self):
+    def __init__(self, executable_path: str, script_path: str):
         """
         Environment for OrderNet RL.
         """
+        self._init_zmq()
+
+        self._executable_path = executable_path
+        self._script_path = script_path
+        self._router_process = None
+
         self._obs_space_shape = (1,)
 
         self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
@@ -42,19 +53,60 @@ class OrderNetEnv(Env):
             low=0, high=1, shape=self._obs_space_shape, dtype=np.float32
         )
 
+        # reward metrics, will track improvement across steps
+        self._violations = 0
+        self._wirelength = 0
+
         self._num_resets = -1  # Becomes 0 after __init__ exits.
         self.reset()
 
     def step(self, action) -> GymStepReturn:
         # The `step()` method must return four values: obs, reward, done, info
-        obs = np.random.rand(*self._obs_space_shape)
-        reward = 1
-        done = False
+        # TODO we get many observations before we get a reward, how to handle that?
+        obs = self._get_observation()
+
+
+        (violations, wirelength) = self._receive_reward()
+        violation_improvement = (
+            1
+            if self.violations == 0
+            else (self._violations - violations) / self._violations
+        )
+        wirelength_improvement = (
+            0
+            if self._wirelength == 0
+            else (self._wirelength - wirelength) / self._wirelength
+        )
+        # reward is the sum of the violation and wirelength improvemnets
+        reward = violation_improvement + wirelength_improvement
+        done = True # for now, there is one step possible
         info = {}
         return obs, reward, done, info
 
     def reset(self) -> GymObs:
         self._num_resets += 1
+
+        if isinstance(self._router_process, subprocess.Popen):
+            try:
+                self._router_process.wait(1)
+            except:
+                self._router_process.terminate()
+        t = self._socket.poll(500) # discard any messages from the killed process
+        for i in range(t):
+            self._socket.recv()
+            self._socket.send_string("ack")
+
+        # Start the router and get the information from the first run
+        # which should generate the initial violations we look to fix
+        self._router_process = subprocess.Popen(
+            [self._executable_path, self._script_path],
+            stdout=subprocess.PIPE
+        )
+
+        # get metrics after the first pass of the detailed router
+        (violations, wirelength) = self._receive_reward()
+        self._violations = violations
+        self._wirelength = wirelength
 
         # reset must return an initial observation value
         return np.random.rand(*self._obs_space_shape)
@@ -64,3 +116,46 @@ class OrderNetEnv(Env):
 
     def close(self):
         pass
+
+    def _init_zmq(self):
+        """
+        Initializes ZMQ communication context and socket
+        """
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.REP)
+        self._socket.bind("tcp://*:5555")
+
+    def _receive_reward(self) -> Tuple[int, int]:
+        """
+        Waits to receive the reward from tritonroute.
+        Returns (numViolations, wireLength)
+        """
+        message = self._socket.recv_json()
+        print("reward: ")
+        print(message)
+        if message["type"] and message["type"] == "reward":
+            self._socket.send_string("ack")
+
+        return (message["data"]["numViolations"], message["data"]["wireLength"])
+
+
+    def _get_observation(self) -> GymObs:
+        message = self._socket.recv_json()
+
+        print("observation: ")
+        print(message)
+
+        if message["type"] is None or message["type"] != "inferenceData":
+            raise TypeError("Expected inference data type")
+        
+        infData = message["data"]
+        nets = infData["nets"]
+        random.shuffle(nets)
+
+        # send the net ordering back to the responder
+        order = {}
+        for i, net in enumerate(nets):
+            order[net["name"]] = i
+
+        self._socket.send_json(order)
+        return  np.random.rand(*self._obs_space_shape)
--- src/drt/orderNet/Train.py
+++ src/drt/orderNet/Train.py
@@ -1,46 +1,67 @@
 import subprocess
+from typing import Dict, List
+from numpy.core.fromnumeric import std
 import zmq
 import os
 import json
 from OrderNetEnv import OrderNetEnv
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_checker import check_env
+import random
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+# context = zmq.Context()
+# socket = context.socket(zmq.REP)
+# socket.bind("tcp://*:5555")
 
-build_dir = os.path.join(os.path.join("/workspaces","OrderNet"), "build")
+build_dir = os.path.join(os.path.join("/workspaces", "OrderNet"), "build")
 
 # subprocess.run(["make", "-j8"], cwd=build_dir)
 
-executable_name = str(os.path.join(build_dir, os.path.join("src","openroad")))
+executable_name = str(os.path.join(build_dir, os.path.join("src", "openroad")))
+script_path = "/home/share/ispd_sample2.tcl"
 
-p = subprocess.Popen([executable_name, "/home/share/ispd_sample2.tcl"], stdout=subprocess.PIPE)
+# p = subprocess.Popen([executable_name, script_path])  # , stdout=subprocess.PIPE)
 
-received_done = False
-while (not received_done):
-  message = socket.recv()
-  message = message.decode("utf-8")
-  if("done" in message):
-    received_done = True
-    continue
-  
-  # otherwise, we get data serialized as json 
-  net_json = json.loads(message)
-  for net in net_json:
-    print(net['name'])
+# received_done = False
+# while not received_done:
+#     message = socket.recv()
+#     message = message.decode("utf-8")
+#     if "done" in message:
+#         received_done = True
+#         continue
 
-  socket.send_string("ack")
+#     # otherwise, we get data serialized as json
+#     net_json = json.loads(message)
+#     print(net_json)
 
-env = OrderNetEnv()
-check_env(env)
+#     if net_json["type"] and net_json["type"] == "reward":
+#         print(net_json["data"])
 
-# model = A2C('policy', env).learn(total_timesteps=100)
+#         socket.send_string("ack")
+#     if net_json["type"] and net_json["type"] == "inferenceData":
+#         nets = net_json["data"]["nets"]
+#         random.shuffle(nets)
 
-p.wait()
-if p.stdout:
-  lines = p.stdout.read().decode("utf-8").split("\n")
-# for line in lines:
-#     print(line)
+#         # send the net ordering back to the responder
+#         order = {}
+#         for i, net in enumerate(nets):
+#             order[net["name"]] = i
 
+#         socket.send_string(json.dumps(order))
+
+env = OrderNetEnv(str(executable_name), script_path)
+# check_env(env)
+
+model = A2C('MlpPolicy', env).learn(total_timesteps=2)
+
+# p.wait()
+
+log_name = "train_log_rand.txt"
+with open(log_name, "w") as f:
+
+    if p.stdout:
+        f.writelines(p.stdout.read().decode("utf-8"))
+        # lines = p.stdout.read().decode("utf-8").split("\n")
+        # for line in lines:
+        #     if "Number of workers" in line:
+        #         print(line)
