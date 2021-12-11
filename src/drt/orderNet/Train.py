b5c22cd968702d7ca99b39bbcf133ff720dc5a70
Robert Viramontes <rviramontes24@gmail.com>
2021-12-11 22:43:35 +0000

    Remove unused stuff.

--- src/drt/orderNet/Train.py
+++ src/drt/orderNet/Train.py
@@ -1,30 +1,20 @@
-import subprocess
 from typing import Dict, List
 from numpy.core.fromnumeric import std
-import zmq
 import os
-import json
 from OrderNetEnv import OrderNetEnv
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_checker import check_env
-import random
-
-# context = zmq.Context()
-# socket = context.socket(zmq.REP)
-# socket.bind("tcp://*:5555")
 
 build_dir = os.path.join(os.path.join("/workspaces", "OrderNet"), "build")
 
 # subprocess.run(["make", "-j8"], cwd=build_dir)
 
 executable_name = str(os.path.join(build_dir, os.path.join("src", "openroad")))
-script_path = "/home/share/ispd_sample2.tcl"
+script_path = "/home/share/ispd_test1.tcl"
 
 env = OrderNetEnv(str(executable_name), script_path)
 # check_env(env)
 
-model = A2C("MlpPolicy", env).learn(total_timesteps=2)
-
-# p.wait()
+model = A2C("MlpPolicy", env).learn(total_timesteps=1)
 
 log_name = "train_log_rand.txt"
