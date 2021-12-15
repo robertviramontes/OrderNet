368789bc190e6c83ef50ad68c1a087695e79247a
Robert Viramontes <rviramontes24@gmail.com>
2021-12-15 05:11:14 +0000

    Clean up to actually allow training the AI model!

--- src/drt/orderNet/OrderNetEnv.py
+++ src/drt/orderNet/OrderNetEnv.py
@@ -26,7 +26,6 @@ import subprocess
 from typing import Dict, List, Optional, Tuple
 from gym import Env, spaces
 import numpy as np
-from numpy.core.fromnumeric import std
 from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
 import zmq
 import random
@@ -55,13 +54,13 @@ class OrderNetEnv(Env):
         self._router_process: Optional[subprocess.Popen] = None
 
         self._num_layers = 9
-        self._obs_space_shape = (9, 200, 200)
-        self._nets_to_order = []
-        self._net_numbering = {}
+        self._obs_space_shape = (9, 250, 250)
+        self._nets_to_order: List[str] = []
+        self._net_numbering: Dict[int, str] = {}
 
-        self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+        self.action_space = spaces.Box(low=0, high=1, shape=(15,), dtype=np.float32)
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=self._obs_space_shape, dtype=np.float32
+            low=0, high=255, shape=self._obs_space_shape, dtype=np.uint8
         )
 
         # reward metrics, will track improvement across steps
@@ -73,8 +72,6 @@ class OrderNetEnv(Env):
 
     def step(self, action) -> GymStepReturn:
         # The `step()` method must return four values: obs, reward, done, info
-        # TODO we get many observations before we get a reward, how to handle that?
-        print("stepping")
 
         # First, write the net ordering to TritonRoute from the action
         self._socket.recv_json()
@@ -97,17 +94,27 @@ class OrderNetEnv(Env):
 
         # reward is the sum of the violation and wirelength improvemnets
         reward = violation_improvement + wirelength_improvement
+        done = violations == 0
 
         # finally, get the observation for the next set of nets to order
         # this updated observation of the state gets returned from the step
         message = self._socket.recv_json()
-        self._get_observation(message)
         self._socket.send_string("ack")
 
-        done = violations == 0
-        info = {}
-        obs = np.random.rand(*self._obs_space_shape)
-        return obs, reward, done, info
+        if message["type"] == "reward" and message["lastInIteration"]:
+            # handle the double reward in the last iteration.
+            message = self._socket.recv_json()
+            self._socket.send_string("ack")
+
+        if message["type"] == "reward":
+            # if we keep getting rewards, indicates we're skipping the searchRepair
+            # process because there are no more violations.
+            obs = np.zeros(self._obs_space_shape)
+            done = True
+        else:
+            obs = self._get_observation(message)
+
+        return obs, reward, done, {}
 
     def reset(self) -> GymObs:
         print("resetting")
@@ -131,8 +138,11 @@ class OrderNetEnv(Env):
         receivedReward = False
         while not receivedReward:
             message = self._socket.recv_json()
-            if "reward" in message["type"] and message["lastInIteration"]:
-                receivedReward = True
+            if "reward" in message["type"]:
+                if message["lastInIteration"]:
+                    receivedReward = True
+                else:
+                    self._socket.send_string("ack")
             else:
                 self._get_first_observation(message)
                 self._socket.send_string("ack")
@@ -144,8 +154,10 @@ class OrderNetEnv(Env):
         # finally get an observation of optimization iteration #1
         if self._num_resets > 0:
             message = self._socket.recv_json()
-            self._get_observation(message)
+            observation = self._get_observation(message)
             message = self._socket.send_string("ack")
+
+            return observation
 
         # reset must return an initial observation value
         return np.random.rand(*self._obs_space_shape)
@@ -174,8 +186,6 @@ class OrderNetEnv(Env):
             raise TypeError("Expected reward type message.")
 
         data = message["data"]
-        print("reward: ")
-        print(data)
 
         self._socket.send_string("ack")
 
@@ -210,7 +220,10 @@ class OrderNetEnv(Env):
         with_pins_in_routebox = []
 
         # filter to only get pins in the range of our routebox!
-        for net in with_pins:
+        for i, net in enumerate(with_pins):
+            if i >= 15:
+                # can only order 15 nets
+                break
             pins = []
             for pin in net["pins"]:
                 if self._in_routebox(
@@ -243,7 +256,7 @@ class OrderNetEnv(Env):
     ) -> np.array:
         pin_array = np.zeros(
             (self._num_layers, routeBoxYRange, routeBoxXRange),
-            dtype=np.float32,
+            dtype=np.uint8,
         )
         for i, net in enumerate(nets):
             self._net_numbering[i] = net["name"]
@@ -279,6 +292,10 @@ class OrderNetEnv(Env):
 
             return (pad_a, pad_b)
 
+        if x_padding_required < 0 or y_padding_required < 0:
+            print("WARNING: EXCEDED BOX SIZE")
+            return np.zeros(self._obs_space_shape, dtype=np.uint8)
+
         (x_pad_left, x_pad_right) = padding_helper(x_padding_required)
         (y_pad_top, y_pad_bottom) = padding_helper(y_padding_required)
         pin_array_padded = np.pad(
@@ -291,13 +308,39 @@ class OrderNetEnv(Env):
 
     def _send_ordering(self, action):
         """Sends the net ordering indicated in action to the router."""
-        random.shuffle(self._nets_to_order)
+
+        action_copy = action
+        np.ndarray.sort(action)
+        action = np.flip(action)
+
+        # order the nets from the network feedback
+        nets_ordered = 0
+        order = {}
+        seen_sorted_vals = []
+        for sorted_val in action:
+            if sorted_val in seen_sorted_vals:
+                continue
+            else:
+                seen_sorted_vals.append(sorted_val)
+
+            match_indices = np.where(action_copy == sorted_val)
+            for i in match_indices[0]:
+                if i < len(self._net_numbering):
+                    order[self._net_numbering[i]] = nets_ordered
+                    nets_ordered += 1
+
+        net_not_yet_ordered = []
+        for net in self._nets_to_order:
+            if net["name"] not in order:
+                net_not_yet_ordered.append(net["name"])
+
+        random.shuffle(net_not_yet_ordered)
+
+        for net in net_not_yet_ordered:
+            order[net] = nets_ordered
+            nets_ordered += 1
 
         # send the net ordering back to the responder
-        order = {}
-        for i, net in enumerate(self._nets_to_order):
-            order[net["name"]] = i
-
         self._socket.send_json(order)
 
     def _get_first_observation(self, message: Dict) -> GymObs:
--- src/drt/orderNet/Train.py
+++ src/drt/orderNet/Train.py
@@ -8,11 +8,12 @@ build_dir = os.path.join(os.path.join("/workspaces", "OrderNet"), "build")
 # subprocess.run(["make", "-j8"], cwd=build_dir)
 
 executable_name = str(os.path.join(build_dir, os.path.join("src", "openroad")))
-script_path = "/home/share/ispd_sample2.tcl"
+script_path = "/home/share/ispd_test1.tcl"
 
 env = OrderNetEnv(str(executable_name), script_path)
 # check_env(env)
 
-model = A2C("MlpPolicy", env).learn(total_timesteps=1)
+model = A2C("CnnPolicy", env).learn(total_timesteps=2500)
 
-log_name = "train_log_rand.txt"
+save_path = os.path.join("/home", "share", "cnn_test1_2500steps.zip")
+model.save(save_path)
