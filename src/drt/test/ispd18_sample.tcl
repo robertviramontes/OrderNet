3d2ac3c8553be4a94371649abca27c9fce8f72d3
Robert <rviramontes@wisc.edu>
2022-02-24 11:47:06 -0600

    Modified driver file.

--- src/drt/test/ispd18_sample.tcl
+++ src/drt/test/ispd18_sample.tcl
@@ -1,11 +1,11 @@
-source "helpers.tcl"
+source "/OrderNet/test/helpers.tcl"
 
-read_lef testcase/ispd18_sample/ispd18_sample.input.lef
-read_def testcase/ispd18_sample/ispd18_sample.input.def
-detailed_route -guide testcase/ispd18_sample/ispd18_sample.input.guide \
-               -output_guide results/ispd18_sample.output.guide.mod \
-               -output_drc results/ispd18_sample.output.drc.rpt \
-               -output_maze results/ispd18_sample.output.maze.log \
+read_lef /OrderNet/src/drt/test/testcase/ispd18_sample/ispd18_sample.input.lef
+read_def /OrderNet/src/drt/test/testcase/ispd18_sample/ispd18_sample.input.def
+detailed_route -guide /OrderNet/src/drt/test/testcase/ispd18_sample/ispd18_sample.input.guide \
+               -output_guide /OrderNet/src/drt/test/results/ispd18_sample.output.guide.mod \
+               -output_drc /OrderNet/src/drt/test/results/ispd18_sample.output.drc.rpt \
+               -output_maze /OrderNet/src/drt/test/results/ispd18_sample.output.maze.log \
                -verbose 0
 
 set def_file [make_result_file ispd18_sample.def]
